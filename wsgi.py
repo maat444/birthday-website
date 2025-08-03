c5e6777c4dfa39c4a76a46b0d69234ed4b0375aa
maat444 <universomaat444@gmail.com>
2025-08-03 16:13:30 -0500

    Update wsgi.py configuration for production

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,15 @@
-from app import app as application
+import sys
+import os
 
-if __name__ == '__main__':
-    application.run()
+# Add the project directory to the path
+path = '/home/maat444/birthday-website'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Configure environment variables
+os.environ['FLASK_ENV'] = 'production'
+os.environ['FLASK_APP'] = 'app.py'
+
+# Import and configure the application
+from app import app as application
+application.secret_key = 'tu_clave_secreta_aqui'

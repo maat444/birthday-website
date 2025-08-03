22ca6fd4bbf0796eb2037855eb04687027306927
maat444 <universomaat444@gmail.com>
2025-08-03 17:02:31 -0500

    Configure for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,4 @@
-import sys
-import os
+from app import app
 
-# Add the project directory to the path
-path = '/home/maat444/birthday-website'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Configure environment variables
-os.environ['FLASK_ENV'] = 'production'
-os.environ['FLASK_APP'] = 'app.py'
-
-# Import and configure the application
-from app import app as application
-application.secret_key = 'tu_clave_secreta_aqui'
+if __name__ == "__main__":
+    app.run()

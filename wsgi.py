19289884b491e52678785bb675b43ea78870e647
maat444 <universomaat444@gmail.com>
2025-08-03 17:18:42 -0500

    Update wsgi.py with environment variables

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from app import app
+import os
+
+# Configurar variables de entorno
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-key')
+app.config['FLASK_ENV'] = os.environ.get('FLASK_ENV', 'production')
 
 if __name__ == "__main__":
     app.run()
